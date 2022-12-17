0a3c06287fd1d2487ed408e92969c159b6e3e2cc
orlando.tyrell <orlandotyrell@gmail.com>
2022-12-17 17:21:18 -0500

    capstone presentation

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+## How to Start
+cd /Users/mindyourbizniz/201-workspace-August-22-204/capstone/capstone\ client
+
+cd /Users/mindyourbizniz/201-workspace-August-22-204/capstone/capstone\ backend
+
+
+T@hmuny54@5703
+
+Dev718
+
+Delete lines 102-106 on landing.js
+
+Orlando.Tyrell@codeimmersives.com
+
+## Folders
+
+- auth: contaiins Passport.js set up/config files
+- db: contains a js file that connects the functionality established in the routes to respective collections in MongoDB
+- frontend: contains all frontend material
+  - public: houses index.js
+  - src: contains javascripts and corresponding stylesheets used to build the site pages (organized by page/component)
+- routes: contains the js files that run the express routing
+
---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require("express");
+require("dotenv").config();
+var path = require("path");
+var logger = require("morgan");
+// cookieParser and session are used to manage your cookies and session variables
+var cookieParser = require("cookie-parser");
+const session = require("express-session");
+// This is used to connect o mongodb
+const MongoStore = require("connect-mongo");
+
+// this is a third party tool to do authentication
+const passport = require("passport");
+
+var indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const projectsRouter = require("./routes/projects");
+const userDataRouter = require("./routes/userData");
+const searchProjectsRouter = require("./routes/searchProjects");
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json()); // body-parser this allows you to use req.body
+app.use(express.urlencoded({ extended: false }));  // this allows you to use req.body
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    // this next property is saving the session data in our DB
+    store: MongoStore.create({
+      mongoUrl: "mongodb+srv://MongoApe212:MongoApe212@codeimmersivescluster.oimxt3p.mongodb.net/?retryWrites=true&w=majority",
+      dbName: "OpTask",
+      collection: "sessions",
+    }),
+    cookie: {
+      maxAge: 7 * 1000 * 60 * 60 * 25, // cookies/sessions will last a week before requiring a re-login
+    },
+  })
+);
+
+require("./auth/passportConfig");
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/projects", projectsRouter);
+app.use("/userData", userDataRouter);
+app.use("/searchProjects", searchProjectsRouter);
+
+module.exports = app;
